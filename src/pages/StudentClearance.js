df9fb02d5dbe991ebdafb425f380826ddbff90da
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-06-04 16:23:38 +0800

    Added view idv student clearance

--- src/pages/StudentClearance.js
+++ src/pages/StudentClearance.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext'; 
+import { useAuth } from '../components/AuthContext'; 
 import { db } from '../firebaseConfig';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import SidebarStudent from '../components/SidebarStudent';
