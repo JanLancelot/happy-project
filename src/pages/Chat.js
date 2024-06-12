6c2f036f58585f7addb380c6908d5df6a0be01df
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-06-12 11:27:43 +0800

    Chat style update - bugfix

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -172,12 +172,13 @@ function Chat() {
         <div className="bg-white p-4 rounded-lg h-96 shadow overflow-y-auto">
           {messages.map((message, index) => {
             const previousMessage = messages[index - 1];
-            const showDateSeparator =
-              !previousMessage ||
-              !moment(message.timestamp.toDate()).isSame(
-                moment(previousMessage.timestamp.toDate()),
-                "day"
-              );
+            const showDateSeparator = 
+            index === 0 || 
+            (previousMessage && 
+             !moment(message.timestamp.toDate()).isSame(
+               moment(previousMessage.timestamp.toDate()), 
+               "day"
+             ));
 
             return (
               <React.Fragment key={message.id}>
