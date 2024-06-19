b008ce8f0e6cf46ee2739ef19f39f713358664f5
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-06-19 15:52:20 +0800

    Added update class

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -56,22 +56,24 @@ function Classes() {
               <th className="py-2">Section Name</th>
               <th className="py-2">Adviser</th>
               <th className="py-2">Education Level</th>
+              <th className="py-2">Actions</th> 
             </tr>
           </thead>
           <tbody>
             {filteredClasses.map((cls) => (
-              <Link
-                to={`/update-class/${cls.id}`}
-                key={cls.id}
-                className="hover:bg-gray-100"
-              >
-                <tr className="cursor-pointer">
-                  <td className="border px-4 py-2">{cls.gradeLevel}</td>
-                  <td className="border px-4 py-2">{cls.sectionName}</td>
-                  <td className="border px-4 py-2">{cls.adviser}</td>
-                  <td className="border px-4 py-2">{cls.educationLevel}</td>
-                </tr>
-              </Link>
+              <tr key={cls.id}>
+                <td className="border px-4 py-2">{cls.gradeLevel}</td>
+                <td className="border px-4 py-2">{cls.sectionName}</td>
+                <td className="border px-4 py-2">{cls.adviser}</td>
+                <td className="border px-4 py-2">{cls.educationLevel}</td>
+                <td className="border px-4 py-2">
+                  <Link to={`/update-class/${cls.id}`}> 
+                    <button className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600">
+                      Update
+                    </button>
+                  </Link>
+                </td> 
+              </tr>
             ))}
           </tbody>
         </table>
