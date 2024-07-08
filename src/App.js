3963afc8ca08a6e8aba8672c36896a6e86b8dfc3
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-07-08 21:16:09 +0800

    Added private routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
 
+import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Teachers from "./pages/Teachers";
@@ -35,36 +36,36 @@ import SendPaymentConfirmationEmail from "./pages/SendPaymentConfirmationEmail";
 
 const router = createBrowserRouter([
   { path: "/", element: <SignIn /> },
-  { path: "/dashboard", element: <Dashboard /> },
-  { path: "/teachers", element: <Teachers /> },
-  { path: "/create-teacher", element: <CreateTeacherPage /> },
-  { path: "/classes", element: <Classes /> },
-  { path: "/create-class", element: <CreateClass /> },
-  { path: "/students", element: <Students /> },
-  { path: "/create-student", element: <CreateStudent /> },
-  { path: "/add-requirement", element: <AddRequirement /> },
-  { path: "/student-clearance", element: <StudentClearance /> },
-  { path: "/approve-clearance-faculty", element: <ApproveClearanceTeachers />},
-  { path: "/user-management", element: <UserManagement /> },
-  { path: "/create-user", element: <CreateUser />},
-  { path: "/add-office-requirement", element: <AddOfficeRequirement />},
-  { path: "/approve-clearance-office", element: <ApproveClearanceOffice />},
-  { path: "/view-classes", element: <ViewClasses />},
-  { path: "/class-details/:classId", element: <ClassDetails />},
-  { path: "/class-details-adviser/:classId", element: <ClassDetailsForAdviser />},
-  { path: "/audit-log", element: <AuditLogs />},
-  { path: "/student-master-list", element: <StudentsMasterList />},
-  { path: "/chat/:recipientId", element: <Chat />},
-  { path: "/view-messages", element: <ViewMessages />},
-  { path: "/view-messages-student", element: <ViewMessagesStudent />},
-  { path: "/disciplinary-records", element: <DisciplinaryRecords />},
-  { path: "/update-class/:classId", element: <UpdateClass />},
-  { path: "/manage-requirements", element: <ManageRequirements />},
-  { path: "/manage-office-requirements", element: <ManageOfficeRequirements />},
-  { path: "/office-clearance-manual", element: <OfficeClearanceManual />},
-  { path: "/forgot-password", element: <ForgotPassword />},
-  { path: "/school-events", element: <SchoolEvents />},
-  { path: "/send-payment-confirmation", element: <SendPaymentConfirmationEmail />}
+  { path: "/forgot-password", element: <ForgotPassword /> },
+  { path: "/dashboard", element: <PrivateRoute><Dashboard /></PrivateRoute> },
+  { path: "/teachers", element: <PrivateRoute><Teachers /></PrivateRoute> },
+  { path: "/create-teacher", element: <PrivateRoute><CreateTeacherPage /></PrivateRoute> },
+  { path: "/classes", element: <PrivateRoute><Classes /></PrivateRoute> },
+  { path: "/create-class", element: <PrivateRoute><CreateClass /></PrivateRoute> },
+  { path: "/students", element: <PrivateRoute><Students /></PrivateRoute> },
+  { path: "/create-student", element: <PrivateRoute><CreateStudent /></PrivateRoute> },
+  { path: "/add-requirement", element: <PrivateRoute><AddRequirement /></PrivateRoute> },
+  { path: "/student-clearance", element: <PrivateRoute><StudentClearance /></PrivateRoute> },
+  { path: "/approve-clearance-faculty", element: <PrivateRoute><ApproveClearanceTeachers /></PrivateRoute>},
+  { path: "/user-management", element: <PrivateRoute><UserManagement /></PrivateRoute> },
+  { path: "/create-user", element: <PrivateRoute><CreateUser /></PrivateRoute>},
+  { path: "/add-office-requirement", element: <PrivateRoute><AddOfficeRequirement /></PrivateRoute>},
+  { path: "/approve-clearance-office", element: <PrivateRoute><ApproveClearanceOffice /></PrivateRoute>},
+  { path: "/view-classes", element: <PrivateRoute><ViewClasses /></PrivateRoute>},
+  { path: "/class-details/:classId", element: <PrivateRoute><ClassDetails /></PrivateRoute>},
+  { path: "/class-details-adviser/:classId", element: <PrivateRoute><ClassDetailsForAdviser /></PrivateRoute>},
+  { path: "/audit-log", element: <PrivateRoute><AuditLogs /></PrivateRoute>},
+  { path: "/student-master-list", element: <PrivateRoute><StudentsMasterList /></PrivateRoute>},
+  { path: "/chat/:recipientId", element: <PrivateRoute><Chat /></PrivateRoute>},
+  { path: "/view-messages", element: <PrivateRoute><ViewMessages /></PrivateRoute>},
+  { path: "/view-messages-student", element: <PrivateRoute><ViewMessagesStudent /></PrivateRoute>},
+  { path: "/disciplinary-records", element: <PrivateRoute><DisciplinaryRecords /></PrivateRoute>},
+  { path: "/update-class/:classId", element: <PrivateRoute><UpdateClass /></PrivateRoute>},
+  { path: "/manage-requirements", element: <PrivateRoute><ManageRequirements /></PrivateRoute>},
+  { path: "/manage-office-requirements", element: <PrivateRoute><ManageOfficeRequirements /></PrivateRoute>},
+  { path: "/office-clearance-manual", element: <PrivateRoute><OfficeClearanceManual /></PrivateRoute>},
+  { path: "/school-events", element: <PrivateRoute><SchoolEvents /></PrivateRoute>},
+  { path: "/send-payment-confirmation", element: <PrivateRoute><SendPaymentConfirmationEmail /></PrivateRoute>}
 ]);
 
 function App() {
